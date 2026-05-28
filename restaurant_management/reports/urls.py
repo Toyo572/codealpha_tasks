472c18e7b5885c9d69f9d78a4d74c41428da74c7
Toyosi Ogundele <ogundeletoyosiogundele@gmail.com>
2026-05-28 00:10:02 -0700

    feat: reports URL path

---
+++ restaurant_management/reports/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from .views import ReportViewSet
+
+router = SimpleRouter()
+router.register("", ReportViewSet, basename="report")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
