1f8b546747620fda49334161aa8e04d4b90bdcea
Toyosi Ogundele <ogundeletoyosiogundele@gmail.com>
2026-05-29 17:50:04 -0700

    feat: add production settings with secure cookie, SSL redirect, and XSS protection flags

---
+++ event_registration/config/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *  # noqa
+
+DEBUG = False
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")  # noqa: F405
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
