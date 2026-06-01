2fb004745830ee1f94f4e28f0a73dec1c29b1d9b
Toyosi Ogundele <ogundeletoyosiogundele@gmail.com>
2026-06-01 13:02:48 -0700

    fix: removed comment

--- event_registration/registrations/urls.py
+++ event_registration/registrations/urls.py
@@ -7,11 +7,8 @@ from .views import (
 )
 
 urlpatterns = [
-    # Attendee
     path("", RegisterForEventView.as_view(), name="register-for-event"),
     path("me/", MyRegistrationsView.as_view(), name="my-registrations"),
     path("<uuid:pk>/cancel/", CancelRegistrationView.as_view(), name="cancel-registration"),
-
-    # Organizer
     path("event/<uuid:event_id>/", EventRegistrationsView.as_view(), name="event-registrations"),
 ]
